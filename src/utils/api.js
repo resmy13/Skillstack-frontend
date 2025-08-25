abfa74d6ff92026f0cef50e0ef94219713be0a16
Ann Resmy Joseph <52193679+resmy13@users.noreply.github.com>
2025-08-25 15:01:34 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -19,7 +19,41 @@ export default api; */
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "// src/utils/api.js
+/*import axios from "axios";
+
+// Create axios instance for API calls
+const api = axios.create({
+  baseURL: "https://skilltracker-134z.onrender.com/api", // Backend base URL
+});
+
+// Add request interceptor to include token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api; */
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://skilltracker-134z.onrender.com/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
+
+/api",
 });
 
 api.interceptors.request.use((config) => {
