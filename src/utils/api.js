d44f220e80785aa1ab4f5452981f5048c1497ced
Ann Resmy Joseph <52193679+resmy13@users.noreply.github.com>
2025-08-25 20:48:17 +0530

    Update api.js
    deleted the commented codes which were not in use

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,42 +1,4 @@
-// src/utils/api.js
-/*import axios from "axios";
 
-// Create axios instance for API calls
-const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend base URL
-});
-
-// Add request interceptor to include token
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default api; */
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "// src/utils/api.js
-/*import axios from "axios";
-
-// Create axios instance for API calls
-const api = axios.create({
-  baseURL: "https://skilltracker-134z.onrender.com/api", // Backend base URL
-});
-
-// Add request interceptor to include token
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default api; */
 import axios from "axios";
 
 const api = axios.create({
