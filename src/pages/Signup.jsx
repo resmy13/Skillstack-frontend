26eabb3feef34b8d931c5d9b47d94a5602d566fa
Ann Resmy Joseph <52193679+resmy13@users.noreply.github.com>
2025-08-25 15:04:31 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -32,7 +32,7 @@ export default function Signup() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://skilltracker-134z.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
